5cfd67267908046753082d2a5fe20cf40726de91
Berserkur <viktor.snaer23@gmail.com>
2022-03-11 10:40:12 +0000

    add time start and stop

--- components/clock/index.js
+++ components/clock/index.js
@@ -6,18 +6,34 @@ export default function index() {
   const [countDown, setCountDown] = useState({
     minutes: 90,
     seconds: 0,
+    clockOn: false,
   });
 
+  useEffect(() => console.log(countDown), [countDown]);
+
   function handleOnClick() {
+    setCountDown((prev) => {
+      return { ...prev, clockOn: !prev.clockOn };
+    });
+
+    handleInterval();
+  }
+
+  function handleInterval() {
     const interval = setInterval(() => {
       setCountDown((prev) => {
-        if (prev.seconds === 0 && prev.minutes > 0) {
-          return { minutes: prev.minutes - 1, seconds: 59 };
+        if (!prev.clockOn) {
+          clearInterval(interval);
+          return {
+            ...prev,
+          };
+        } else if (prev.seconds === 0 && prev.minutes > 0) {
+          return { minutes: prev.minutes - 1, seconds: 59, clockOn: true };
         } else if (prev.seconds > 0) {
           return { ...prev, seconds: prev.seconds - 1 };
         } else if (prev.seconds === 0 && prev.minutes === 0) {
           clearInterval(interval);
-          return { minutes: 90, seconds: 0 };
+          return { minutes: 90, seconds: 0, clockOn: false };
         }
       });
     }, 1000);
